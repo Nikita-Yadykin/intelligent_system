f28fd08d62898b334218451f2a78462f07c6c01e
Никита Ядыкин <jadykinnikita@rambler.ru>
2024-04-03 01:46:25 +0300

    Добавлена логика в файл data_processing.py

---
+++ data_processing.py
@@ -0,0 +1,19 @@
+import mysql.connector
+import pandas as pd
+from interface import get_user_input
+
+def prepare_data(ui):
+    user_input = get_user_input(ui)
+    data = pd.DataFrame([user_input],
+                        columns=['1', '2', '3', '4', '5', '6', '1.2', '2.2', '3.2', '4.2', '1.3', '2.3', '3.3'])
+
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password",
+        database="test"
+    )
+
+    data_ml_table = pd.read_sql_query('SELECT * FROM ml_table_all_random', connection)
+
+    return data, data_ml_table
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from ui_form import Ui_Widget
-import mysql.connector
-import pandas as pd
-from interface import get_user_input, update_result_label
+from interface import update_result_label, get_user_input  # Импорт функции get_user_input
+from data_processing import prepare_data
 from machine_learning import train_and_predict
 
 class YourMainWindow(QMainWindow):
@@ -15,19 +14,12 @@ class YourMainWindow(QMainWindow):
 
     def calculate_risk(self):
         try:
+            data, data_ml_table = prepare_data(self.ui)
+
+            # Получение ввода пользователя
             user_input = get_user_input(self.ui)
-            data = pd.DataFrame([user_input],
-                                columns=['1', '2', '3', '4', '5', '6', '1.2', '2.2', '3.2', '4.2', '1.3', '2.3', '3.3'])
 
-            connection = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                password="password",
-                database="test"
-            )
-
-            data_ml_table = pd.read_sql_query('SELECT * FROM ml_table_all_random', connection)
-
+            # Предсказание и анализ риска
             prediction, total_sum, y_test, y_pred = train_and_predict(data_ml_table, data, user_input)
 
             if 13 <= total_sum <= 20:
@@ -45,7 +37,6 @@ class YourMainWindow(QMainWindow):
 
         except Exception as e:
             self.ui.label_70.setText(f"Ошибка: {e}")
-
 
 def main():
     app = QApplication(sys.argv)
